2649efec1c6a4f17f1959766d5175b0fb675374e
sunxiaoda <sunxiaoda@baidu.com>
2018-12-04 19:21:26 +0800

    初始提交

---
+++ readme.md
@@ -0,0 +1,29 @@
+#查看APP使用FD情况, FD泄漏
+---
+##说明
+
+类似以下的Crash，做Android的同学应该都不陌生，尤其是华为抓机。一般Android系统的FD限制多为1024，当超过该值，就会引起崩溃，堆栈包括但不限于以下几种：
+
+* FD leaks, File Description Leaks, Too many open files, error 24
+* FORTIFY: FD_SET: file descriptor >= FD_SETSIZE
+* pthread_create (1040KB stack) failed: Out of memory
+
+那么，赶紧看看系统的FD占用情况吧，也许罪魁祸首就是它
+
+##使用方法
+
+```shell
+./watch_fd.sh  com.xxx.zzz
+```
+##输出
+```shell
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+-e Watching App com.xxx.zzz<pid>, Current FD 364. Press CTRL+C to stop.
+```
+	
+	
---
+++ watch_fd.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# useage: ./watch_fd.sh <application_id> [delay_sec = 1]
+
+APPID=${1:?PackegeName missing}
+DELAY=$(( ${2:-1} ))
+
+CLEAR='\033[0m'
+YELLOW='\033[33m'
+RED='\033[31m'
+GREEN='\033[32m'
+
+FD_LIMIT=$(( $(adb shell ulimit -n) ))
+WARN_THRESHOLD=$(( ${FD_LIMIT} / 2 ))
+ERROR_THRESHOLD=$(( ${FD_LIMIT} / 3 * 2 ))
+echo -e "FD_LIMIT ${FD_LIMIT}, WILL WARN WHEN FD > ${YELLOW}${WARN_THRESHOLD}${CLEAR} ADN FD > ${RED}${WARN_THRESHOLD}${CLEAR}"
+
+APP_PID=$(( $(adb shell ps | grep -e "${APPID}$" | awk '{print $2}') ))
+
+while true; 
+do
+	APP_FD=$(( $(adb shell run-as ${APPID} ls -l /proc/${APP_PID}/fd | wc -l) ))
+	if [ $APP_FD -ge $WARN_THRESHOLD ]
+	then
+		echo -e "Watching App ${YELLOW}${APPID}<${APP_PID}>${CLEAR}, Current FD ${YELLOW}${APP_FD}${CLEAR}. Press CTRL+C to stop."
+	elif [ $APP_FD -ge $ERROR_THRESHOLD ]
+	then
+		echo -e "Watching App ${RED}${APPID}<${APP_PID}>${CLEAR}, Current FD ${RED}${APP_FD}${CLEAR}. Press CTRL+C to stop."
+	else
+		echo -e "Watching App ${GREEN}${APPID}<${APP_PID}>${CLEAR}, Current FD ${GREEN}${APP_FD}${CLEAR}. Press CTRL+C to stop."
+	fi
+	sleep ${2}
+done
+
+
+
+
